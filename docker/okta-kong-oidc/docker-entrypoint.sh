1720144f404ad842da4d5082294a532ff06e260e
Andy March <mail@andymarch.co.uk>
2018-11-30 14:38:12 +0000

    Updated Kong image
    Updated image to base from Kong, included installation of Lua as a pre-req for OIDC.

--- docker/okta-kong-oidc/docker-entrypoint.sh
+++ docker/okta-kong-oidc/docker-entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 set -e
 
-# Disabling nginx daemon mode
-export KONG_NGINX_DAEMON="off"
+export KONG_NGINX_DAEMON=off
 
-# Setting default prefix (override any existing variable)
-export KONG_PREFIX="/usr/local/kong"
+if [[ "$1" == "kong" ]]; then
+  PREFIX=${KONG_PREFIX:=/usr/local/kong}
+  mkdir -p $PREFIX
 
-# Prepare Kong prefix
-if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
-	kong prepare -p "/usr/local/kong"
+  if [[ "$2" == "docker-start" ]]; then
+    kong prepare -p $PREFIX
+
+    exec /usr/local/openresty/nginx/sbin/nginx \
+      -p $PREFIX \
+      -c nginx.conf
+  fi
 fi
 
 exec "$@"
